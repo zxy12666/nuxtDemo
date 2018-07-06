9f064dc54ede0b7c411f04f0c9ab2556a5d1c5f3
zhangxiangyu <zhangxiangyu@jianhuabm.com>
2018-07-06 10:46:11 +0800

    ~

---
+++ pages/asyncData.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>姓名：{{info.name}}</h1>
+    <h2>年龄：{{info.age}}</h2>
+    <h2>兴趣：{{info.interest}}</h2>
+    <li><nuxt-link :to="{name:'index'}">HOME</nuxt-link></li>
+  </div>
+</template>
+<script>
+  import axios from 'axios'
+  export default {
+    data(){
+      return {
+        name:'hello World',
+      }
+    },
+    async asyncData(){
+      let {data}=await axios.get('https://api.myjson.com/bins/8gdmr')
+      return {info: data}
+
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
+    <div class="diss"></div>
     <ul>
       <li><nuxt-link :to="{name:'index'}">HOME</nuxt-link></li>
       <li><nuxt-link :to="{name:'about'}">ABOUT</nuxt-link></li>
       <li><nuxt-link :to="{name:'news'}">NEWS</nuxt-link></li>
+      <li><nuxt-link :to="{name:'asyncData'}">asyncData</nuxt-link></li>
     </ul>
   </div>
 </template>
@@ -19,5 +21,10 @@ export default {
 </script>
 
 <style>
-
+  .diss{
+    width: 500px;
+    height:500px;
+    background-image: url('~static/logo.png');
+    background-size: cover;
+  }
 </style>
